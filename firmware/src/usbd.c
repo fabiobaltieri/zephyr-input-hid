87ba38bfb912340eb7bcd012199e6adba182325a
Fabio Baltieri <fabio.baltieri@gmail.com>
2024-06-04 19:58:11 +0100

    fw: usbd refactor

--- firmware/src/usbd.c
+++ firmware/src/usbd.c
@@ -1,6 +1,7 @@
 #include <zephyr/device.h>
 #include <zephyr/kernel.h>
 #include <zephyr/usb/usbd.h>
+#include <zephyr/input/input.h>
 
 LOG_MODULE_REGISTER(usbd_app, LOG_LEVEL_INF);
 
@@ -10,20 +11,20 @@ LOG_MODULE_REGISTER(usbd_app, LOG_LEVEL_INF);
 #define USBD_PID 0x0007
 #define USBD_MAX_POWER 250
 
-USBD_DEVICE_DEFINE(sample_usbd,
+USBD_DEVICE_DEFINE(app_usbd,
 		   DEVICE_DT_GET(DT_NODELABEL(zephyr_udc0)),
 		   USBD_VID, USBD_PID);
 
-USBD_DESC_LANG_DEFINE(sample_lang);
-USBD_DESC_MANUFACTURER_DEFINE(sample_mfr, CONFIG_APP_MANUFACTURER_NAME);
-USBD_DESC_PRODUCT_DEFINE(sample_product, CONFIG_APP_DEVICE_NAME);
-USBD_DESC_SERIAL_NUMBER_DEFINE(sample_sn);
+USBD_DESC_LANG_DEFINE(app_usb_lang);
+USBD_DESC_MANUFACTURER_DEFINE(app_usb_mfr, CONFIG_APP_MANUFACTURER_NAME);
+USBD_DESC_PRODUCT_DEFINE(app_usb_product, CONFIG_APP_DEVICE_NAME);
+USBD_DESC_SERIAL_NUMBER_DEFINE(app_usb_sn);
 
 static const uint8_t attributes = USB_SCD_REMOTE_WAKEUP;
 
-USBD_CONFIGURATION_DEFINE(sample_fs_config, attributes, USBD_MAX_POWER);
+USBD_CONFIGURATION_DEFINE(app_usb_fs_config, attributes, USBD_MAX_POWER);
 
-USBD_CONFIGURATION_DEFINE(sample_hs_config, attributes, USBD_MAX_POWER);
+USBD_CONFIGURATION_DEFINE(app_usb_hs_config, attributes, USBD_MAX_POWER);
 
 static int register_fs_classes(struct usbd_context *uds_ctx)
 {
@@ -55,9 +56,9 @@ static int register_hs_classes(struct usbd_context *uds_ctx)
 	return 0;
 }
 
-static int sample_add_configuration(struct usbd_context *uds_ctx,
-				    const enum usbd_speed speed,
-				    struct usbd_config_node *config)
+static int app_usb_add_configuration(struct usbd_context *uds_ctx,
+				     const enum usbd_speed speed,
+				     struct usbd_config_node *config)
 {
 	int err;
 
@@ -92,54 +93,54 @@ struct usbd_context *usbd_init_device(void)
 {
 	int err;
 
-	err = usbd_add_descriptor(&sample_usbd, &sample_lang);
+	err = usbd_add_descriptor(&app_usbd, &app_usb_lang);
 	if (err) {
 		LOG_ERR("Failed to initialize language descriptor (%d)", err);
 		return NULL;
 	}
 
-	err = usbd_add_descriptor(&sample_usbd, &sample_mfr);
+	err = usbd_add_descriptor(&app_usbd, &app_usb_mfr);
 	if (err) {
 		LOG_ERR("Failed to initialize manufacturer descriptor (%d)", err);
 		return NULL;
 	}
 
-	err = usbd_add_descriptor(&sample_usbd, &sample_product);
+	err = usbd_add_descriptor(&app_usbd, &app_usb_product);
 	if (err) {
 		LOG_ERR("Failed to initialize product descriptor (%d)", err);
 		return NULL;
 	}
 
-	err = usbd_add_descriptor(&sample_usbd, &sample_sn);
+	err = usbd_add_descriptor(&app_usbd, &app_usb_sn);
 	if (err) {
 		LOG_ERR("Failed to initialize SN descriptor (%d)", err);
 		return NULL;
 	}
 
-	if (usbd_caps_speed(&sample_usbd) == USBD_SPEED_HS) {
-		err = sample_add_configuration(&sample_usbd, USBD_SPEED_HS, &sample_hs_config);
+	if (usbd_caps_speed(&app_usbd) == USBD_SPEED_HS) {
+		err = app_usb_add_configuration(&app_usbd, USBD_SPEED_HS, &app_usb_hs_config);
 		if (err) {
 			LOG_ERR("Failed to add High-Speed configuration");
 			return NULL;
 		}
 	}
 
-	err = sample_add_configuration(&sample_usbd, USBD_SPEED_FS, &sample_fs_config);
+	err = app_usb_add_configuration(&app_usbd, USBD_SPEED_FS, &app_usb_fs_config);
 	if (err) {
 		LOG_ERR("Failed to add Full-Speed configuration");
 		return NULL;
 	}
 
-	err = usbd_init(&sample_usbd);
+	err = usbd_init(&app_usbd);
 	if (err) {
 		LOG_ERR("Failed to initialize device support");
 		return NULL;
 	}
 
-	return &sample_usbd;
+	return &app_usbd;
 }
 
-static int global_usbd_enable(void)
+static int app_usbd_enable(void)
 {
 	struct usbd_context *usbd;
 	int ret;
@@ -158,4 +159,19 @@ static int global_usbd_enable(void)
 
 	return 0;
 }
-SYS_INIT(global_usbd_enable, APPLICATION, 0);
+SYS_INIT(app_usbd_enable, APPLICATION, 0);
+
+#if 0
+static void input_resume_cb(struct input_event *evt)
+{
+        if (!evt->sync) {
+                return;
+        }
+
+	if (usbd_is_suspended(&app_usbd)) {
+		usbd_wakeup_request(&app_usbd);
+	}
+
+}
+INPUT_CALLBACK_DEFINE(NULL, input_resume_cb);
+#endif
