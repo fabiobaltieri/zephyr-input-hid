da302d99e58afdf2cf798aa832656f68e34becf4
Fabio Baltieri <fabio.baltieri@gmail.com>
2024-06-04 19:02:08 +0100

    fw: make input activity blinker selectable

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -7,6 +7,15 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 static struct k_sem blink_sem = Z_SEM_INITIALIZER(blink_sem, 0, 10);
 
+#if DT_NODE_EXISTS(DT_NODELABEL(led_input_blink))
+#define BLINKER_NODE DT_NODELABEL(led_input_blink)
+static const struct device *leds = DEVICE_DT_GET(DT_PARENT(BLINKER_NODE));
+static const uint32_t blinker_led = DT_NODE_CHILD_IDX(BLINKER_NODE);
+#else
+static const struct device *leds;
+static const uint32_t blinker_led;
+#endif
+
 static void blink_input_cb(struct input_event *evt)
 {
 	if (!evt->sync) {
@@ -19,20 +28,15 @@ INPUT_CALLBACK_DEFINE(NULL, blink_input_cb);
 
 int main(void)
 {
-	static const struct device *leds = DEVICE_DT_GET(
-			DT_COMPAT_GET_ANY_STATUS_OKAY(gpio_leds));
-
-	if (!device_is_ready(leds)) {
-		return -ENODEV;
-	}
-
 	while (true) {
 		k_sem_take(&blink_sem, K_FOREVER);
 
-		led_on(leds, 0);
-		k_sleep(K_MSEC(30));
-		led_off(leds, 0);
-		k_sleep(K_MSEC(30));
+		if (leds != NULL) {
+			led_on(leds, blinker_led);
+			k_sleep(K_MSEC(30));
+			led_off(leds, blinker_led);
+			k_sleep(K_MSEC(30));
+		}
 	}
 
 	return 0;
