26fa41aacc172d89aa5e4172bd72a9d716aa1b7d
Fabio Baltieri <fabio.baltieri@gmail.com>
2024-06-26 19:46:11 +0100

    fw: add paw-testboard

---
+++ firmware/boards/balto/paw_testboard/board.cmake
@@ -0,0 +1,6 @@
+board_runner_args(nrfjprog "--softreset")
+board_runner_args(jlink "--device=nRF52833_xxAA" "--speed=4000")
+
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
