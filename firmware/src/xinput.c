ea4b50cd1a35166f75a1223faa2bbdc5ad52fc74
Fabio Baltieri <fabio.baltieri@gmail.com>
2024-07-26 20:23:58 +0100

    fw: add xinput out support

--- firmware/src/xinput.c
+++ firmware/src/xinput.c
@@ -34,6 +34,7 @@ struct xinput_desc {
 
 struct xinput_config {
 	struct net_buf_pool *pool_in;
+	struct net_buf_pool *pool_out;
 	struct usbd_class_data *c_data;
 	struct xinput_desc *desc;
 	const struct usb_desc_header **fs_desc;
@@ -41,6 +42,8 @@ struct xinput_config {
 };
 
 struct xinput_data {
+	const struct device *dev;
+	struct k_work out_work;
 };
 
 #define XINPUT_BUF_COUNT 4
@@ -152,10 +155,22 @@ static int xinput_request_handler(struct usbd_class_data *c_data,
 				  struct net_buf *buf, int err)
 {
 	struct usbd_context *uds_ctx = usbd_class_get_ctx(c_data);
-	struct udc_buf_info *bi = NULL;
+	const struct device *dev = usbd_class_get_private(c_data);
+	const struct xinput_config *cfg = dev->config;
+	struct xinput_data *data = dev->data;
+	struct udc_buf_info *bi;
 
 	bi = net_buf_user_data(buf);
-	LOG_INF("%p -> ep 0x%02x, len %u, err %d", c_data, bi->ep, buf->len, err);
+
+	if (bi->ep == cfg->desc->if0_out_ep.bEndpointAddress) {
+		LOG_HEXDUMP_INF(buf->data, buf->len, "data");
+
+		k_work_submit(&data->out_work);
+	} else if (bi->ep == cfg->desc->if0_in_ep.bEndpointAddress) {
+		LOG_DBG("ep %02x done", bi->ep);
+	} else {
+		LOG_INF("%p -> ep 0x%02x, len %u, err %d", c_data, bi->ep, buf->len, err);
+	}
 
 	return usbd_ep_buf_free(uds_ctx, buf);
 }
@@ -173,7 +188,22 @@ static void *xinput_get_desc(struct usbd_class_data *const c_data,
 	return cfg->fs_desc;
 }
 
-static int xinput_init(struct usbd_class_data *c_data)
+static void xinput_enable(struct usbd_class_data *const c_data)
+{
+	const struct device *dev = usbd_class_get_private(c_data);
+	struct xinput_data *data = dev->data;
+
+	k_work_submit(&data->out_work);
+
+	LOG_INF("%s\n", __func__);
+}
+
+static void xinput_disable(struct usbd_class_data *const c_data)
+{
+	LOG_INF("%s\n", __func__);
+}
+
+static int xinput_usb_init(struct usbd_class_data *c_data)
 {
 	LOG_INF("%s\n", __func__);
 	return 0;
@@ -181,11 +211,13 @@ static int xinput_init(struct usbd_class_data *c_data)
 
 struct usbd_class_api xinput_api = {
 	.update = xinput_update,
+	.enable = xinput_enable,
+	.disable = xinput_disable,
 	.control_to_host = xinput_control_to_host,
 	.control_to_dev = xinput_control_to_dev,
 	.request = xinput_request_handler,
 	.get_desc = xinput_get_desc,
-	.init = xinput_init,
+	.init = xinput_usb_init,
 };
 
 static const struct usbd_cctx_vendor_req xinput_vregs = USBD_VENDOR_REQ(0x01);
@@ -206,11 +238,39 @@ static void xinput_send_report(const struct device *dev,
 
 	bi = udc_get_buf_info(buf);
 	memset(bi, 0, sizeof(struct udc_buf_info));
+
 	bi->ep = cfg->desc->if0_in_ep.bEndpointAddress;
 
 	ret = usbd_ep_enqueue(cfg->c_data, buf);
 	if (ret) {
 		net_buf_unref(buf);
+		return;
+	}
+}
+
+static void xinput_out_handler(struct k_work *work)
+{
+	struct xinput_data *data = CONTAINER_OF(work, struct xinput_data, out_work);
+	const struct device *dev = data->dev;
+	const struct xinput_config *cfg = dev->config;
+	struct usbd_class_data *c_data = cfg->c_data;
+
+        struct net_buf *buf;
+        struct udc_buf_info *bi;
+
+	buf = net_buf_alloc(cfg->pool_out, K_NO_WAIT);
+        if (!buf) {
+                return;
+        }
+
+	bi = udc_get_buf_info(buf);
+        memset(bi, 0, sizeof(struct udc_buf_info));
+
+        bi->ep = cfg->desc->if0_out_ep.bEndpointAddress;
+
+	if (usbd_ep_enqueue(c_data, buf)) {
+		net_buf_unref(buf);
+		LOG_ERR("Failed to enqueue buffer");
 		return;
 	}
 }
@@ -224,12 +284,32 @@ static void xinput_cb(const struct device *dev, struct input_event *evt)
 	report.report_size = sizeof(report);
 
 	switch (evt->code) {
-	case INPUT_KEY_A:
-		WRITE_BIT(report.buttons_1, 0, evt->value);
+	case INPUT_BTN_SOUTH:
+		WRITE_BIT(report.buttons_1, 4, evt->value);
+		break;
+	case INPUT_BTN_EAST:
+		WRITE_BIT(report.buttons_1, 5, evt->value);
+		break;
+	case INPUT_BTN_NORTH:
+		WRITE_BIT(report.buttons_1, 6, evt->value);
+		break;
+	case INPUT_BTN_WEST:
+		WRITE_BIT(report.buttons_1, 7, evt->value);
 		break;
 	}
 
 	xinput_send_report(dev, &report);
+}
+
+static int xinput_init(const struct device *dev)
+{
+	struct xinput_data *data = dev->data;
+
+	data->dev = dev;
+
+	k_work_init(&data->out_work, xinput_out_handler);
+
+	return 0;
 }
 
 #define XINPUT_CB(node_id, prop, idx, n)						\
@@ -249,6 +329,8 @@ static void xinput_cb(const struct device *dev, struct input_event *evt)
 											\
 	NET_BUF_POOL_DEFINE(xinput_buf_pool_in_##n, XINPUT_BUF_COUNT, 0,		\
 		    sizeof(struct udc_buf_info), NULL);					\
+	NET_BUF_POOL_DEFINE(xinput_buf_pool_out_##n, XINPUT_BUF_COUNT, 32,		\
+		    sizeof(struct udc_buf_info), NULL);					\
 											\
 	struct xinput_desc xinput_desc_##n = XINPUT_DESC_INIT();			\
 	const static struct usb_desc_header *xinput_fs_desc_##n[] =			\
@@ -258,6 +340,7 @@ static void xinput_cb(const struct device *dev, struct input_event *evt)
 											\
 	static const struct xinput_config xinput_cfg_##n = {				\
 		.pool_in = &xinput_buf_pool_in_##n,					\
+		.pool_out = &xinput_buf_pool_out_##n,					\
 		.c_data = &xinput_##n,							\
 		.desc = &xinput_desc_##n,						\
 		.fs_desc = xinput_fs_desc_##n,						\
@@ -266,7 +349,7 @@ static void xinput_cb(const struct device *dev, struct input_event *evt)
 											\
 	static struct xinput_data xinput_data_##n;					\
 											\
-	DEVICE_DT_INST_DEFINE(n, NULL, NULL,						\
+	DEVICE_DT_INST_DEFINE(n, xinput_init, NULL,					\
 			      &xinput_data_##n, &xinput_cfg_##n,			\
 			      POST_KERNEL, CONFIG_INPUT_INIT_PRIORITY, NULL);
 
