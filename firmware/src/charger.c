5301ed5dcf9a45c94ebcd7e14d3c6821fbab1ff3
Fabio Baltieri <fabio.baltieri@gmail.com>
2024-07-28 22:21:31 +0100

    fw: add charger events

--- firmware/src/charger.c
+++ firmware/src/charger.c
@@ -2,6 +2,8 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
+#include "event.h"
+
 LOG_MODULE_REGISTER(charger, LOG_LEVEL_INF);
 
 static const struct device *charger = DEVICE_DT_GET_OR_NULL(DT_NODELABEL(charger));
@@ -13,6 +15,8 @@ K_WORK_DELAYABLE_DEFINE(charging_dwork, charging_handler);
 static void charging_handler(struct k_work *work)
 {
 	union charger_propval val0, val1;
+	static enum event_code last_code = EVENT_CHARGER_DISCONNECTED;
+	static enum event_code code;
 	int ret;
 
 	ret = charger_get_prop(charger, CHARGER_PROP_ONLINE, &val0);
@@ -25,8 +29,21 @@ static void charging_handler(struct k_work *work)
 		goto out;
 	}
 
-	if (val0.online) {
-		LOG_INF("charger status: %d", val1.status);
+	if (val0.online == CHARGER_ONLINE_OFFLINE) {
+		code = EVENT_CHARGER_DISCONNECTED;
+	} else if (val1.status == CHARGER_STATUS_CHARGING) {
+		code = EVENT_CHARGER_CHARGING;
+	} else if (val1.status == CHARGER_STATUS_FULL) {
+		code = EVENT_CHARGER_FULL;
+	} else {
+		LOG_WRN("unknown charger state: oneline=%d status=%d",
+			val0.online, val1.status);
+		goto out;
+	}
+
+	if (code != last_code) {
+		event(code);
+		last_code = code;
 	}
 
 out:
