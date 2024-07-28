58c017d21b80cf11718502ff64d9dc2472e5ce39
Fabio Baltieri <fabio.baltieri@gmail.com>
2024-07-28 22:23:31 +0100

    fw: add panic.c

---
+++ firmware/src/panic.c
@@ -0,0 +1,14 @@
+#include <zephyr/fatal.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/logging/log_ctrl.h>
+#include <zephyr/sys/reboot.h>
+
+LOG_MODULE_REGISTER(panic, LOG_LEVEL_INF);
+
+void k_sys_fatal_error_handler(unsigned int reason,
+			       const struct arch_esf *esf)
+{
+	LOG_PANIC();
+	LOG_ERR("Fatal error - reboot");
+	sys_reboot(0);
+}
