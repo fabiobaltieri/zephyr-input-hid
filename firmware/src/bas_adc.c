eccc317b4e129fad6af80eac012109b17342ac2c
Fabio Baltieri <fabio.baltieri@gmail.com>
2024-08-30 09:02:48 +0100

    fw: use the new sensor/fuel gauge based soc code

--- firmware/src/bas_adc.c
+++ firmware/src/bas_adc.c
@@ -1,77 +1,43 @@
 #include <zephyr/bluetooth/services/bas.h>
 #include <zephyr/device.h>
-#include <zephyr/drivers/adc.h>
+#include <zephyr/drivers/fuel_gauge.h>
 #include <zephyr/init.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/sys/util.h>
 
 LOG_MODULE_REGISTER(bas_adc, LOG_LEVEL_INF);
 
 #define SOC_UPDATE_SECS 10
 
-#define VBATT_NODE DT_NODELABEL(vbatt)
-#define VBATT_FULL_MV (4200 - 50)
-#define VBATT_EMPTY_MV (3000 + 50)
-static const struct adc_dt_spec vbatt = ADC_DT_SPEC_GET(VBATT_NODE);
+static const struct device *fuel_gauge = DEVICE_DT_GET_ONE(zephyr_fuel_gauge_composite);
 
-static void soc_adc_handler(struct k_work *work);
+static void bas_adc_handler(struct k_work *work);
 
-K_WORK_DELAYABLE_DEFINE(soc_adc_dwork, soc_adc_handler);
+K_WORK_DELAYABLE_DEFINE(bas_adc_work, bas_adc_handler);
 
-static void soc_adc_handler(struct k_work *work)
+static void bas_adc_handler(struct k_work *work)
 {
-	int16_t buf;
-	struct adc_sequence sequence = {
-		.buffer = &buf,
-		.buffer_size = sizeof(buf),
-	};
-	int32_t vbatt_mv;
-	int soc;
-	int err;
+	union fuel_gauge_prop_val val1;
+	union fuel_gauge_prop_val val2;
 
-	adc_sequence_init_dt(&vbatt, &sequence);
+	fuel_gauge_get_prop(fuel_gauge, FUEL_GAUGE_RELATIVE_STATE_OF_CHARGE, &val1);
+	fuel_gauge_get_prop(fuel_gauge, FUEL_GAUGE_VOLTAGE, &val2);
 
-	err = adc_read(vbatt.dev, &sequence);
-	if (err < 0) {
-		LOG_ERR("Could not read (%d)", err);
-		return;
-	}
+	LOG_INF("vbatt_mv=%d soc=%d", val2.voltage / 1000, val1.relative_state_of_charge);
 
-	vbatt_mv = buf;
-	adc_raw_to_millivolts_dt(&vbatt, &vbatt_mv);
+	bt_bas_set_battery_level(val1.relative_state_of_charge);
 
-	vbatt_mv = vbatt_mv *
-		DT_PROP(VBATT_NODE, full_ohms) /
-		DT_PROP(VBATT_NODE, output_ohms);
-
-	soc = (vbatt_mv - VBATT_EMPTY_MV) * 100 /
-		(VBATT_FULL_MV - VBATT_EMPTY_MV);
-	soc = CLAMP(soc, 0, 100);
-
-	LOG_INF("update_soc: vbatt_mv=%d soc=%d", vbatt_mv, soc);
-
-	bt_bas_set_battery_level(soc);
-
-	k_work_schedule(&soc_adc_dwork, K_SECONDS(SOC_UPDATE_SECS));
+	k_work_schedule(&bas_adc_work, K_SECONDS(SOC_UPDATE_SECS));
 }
 
 static int bas_soc_adc_init(void)
 {
-	int err;
-
-	if (!device_is_ready(vbatt.dev)) {
-		LOG_ERR("ADC controller device not ready");
+	if (!device_is_ready(fuel_gauge)) {
+		LOG_ERR("fuel gauge device is not ready");
 		return -ENODEV;
 	}
 
-	err = adc_channel_setup_dt(&vbatt);
-	if (err < 0) {
-		LOG_ERR("Could not setup ADC channel (%d)", err);
-		return err;
-	}
-
-	soc_adc_handler(&soc_adc_dwork.work);
+	bas_adc_handler(&bas_adc_work.work);
 
 	return 0;
 }
