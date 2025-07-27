8d0dba666581e1f473295659651adbd42b19e14c
Fabio Baltieri <fabio.baltieri@gmail.com>
2025-07-27 22:09:26 +0100

    fw: kbd matrix default to nrfjprog

--- firmware/boards/balto/kbd_matrix/board.cmake
+++ firmware/boards/balto/kbd_matrix/board.cmake
@@ -1,5 +1,5 @@
 board_runner_args(jlink "--device=nRF52832_xxAA" "--speed=4000")
 
-include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
