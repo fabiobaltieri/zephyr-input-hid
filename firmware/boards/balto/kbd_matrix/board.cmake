ebe39700f4666895b0376646013612776684e395
Fabio Baltieri <fabio.baltieri@gmail.com>
2025-08-03 23:32:07 +0100

    fw: board: default to nrfutil

--- firmware/boards/balto/kbd_matrix/board.cmake
+++ firmware/boards/balto/kbd_matrix/board.cmake
@@ -1,5 +1,6 @@
 board_runner_args(jlink "--device=nRF52832_xxAA" "--speed=4000")
 
+include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
