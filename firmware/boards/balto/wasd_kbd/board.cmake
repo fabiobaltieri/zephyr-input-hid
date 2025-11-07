ee53684a9fe8d7e7d4d0da89923885c51105453e
Fabio Baltieri <fabio.baltieri@gmail.com>
2025-11-07 22:14:52 +0000

    fw: wasd: default to dfu-util

--- firmware/boards/balto/wasd_kbd/board.cmake
+++ firmware/boards/balto/wasd_kbd/board.cmake
@@ -1,5 +1,5 @@
 board_runner_args(jlink "--device=STM32C071GB" "--speed=4000")
 board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
 
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
